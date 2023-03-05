80bb4dca9a397b09ed201e97c6f71ffc512826bb
student <student@intranet.proweb.uz>
2023-03-05 16:51:38 +0500

    project

--- src/components/ThisDay/index.jsx
+++ src/components/ThisDay/index.jsx
@@ -9,15 +9,15 @@ import WeatherIcons from '../../assets/icons/WeatherIcons';
 const ThisDay = () => {
   const weather = useSelector(selectWeather);
   const {current} = weather
-  console.log(current.weather[0])
+  console.log(current.weather[0]&&current.weather[0])
   return (
     <div className={styles['thisDay']}>
       <div className={styles['thisDay__topBlock']}>
         <div className={styles['thisDay__wrapper']}>
-          <h2 className={styles['thisDay__temp']}>{Math.trunc(weather.current.temp)}°</h2>
+          <h2 className={styles['thisDay__temp']}>{weather.current.temp && Math.trunc(weather.current.temp)}°</h2>
           <h3 className={styles['thisDay__day']}>Сегодня</h3>
         </div>
-        <WeatherIcons id={current.weather[0].description} size={`100%`}/>
+        <WeatherIcons id={current.weather[0].description && current.weather[0].description} size={`100%`}/>
       </div>
       <div className={styles['thisDay__bottomBlock']}>
           <p className={styles['thisDay__info']}>Время: {new Date().getHours()} : {new Date().getMinutes()}</p>
